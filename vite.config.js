18e06f65cffdf5e152914a8fbdd8b40703be52aa
Ivan <racioglobus@yandex.ru>
2023-10-30 02:36:03 +0300

    add homepage2

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "ratioglobus",
   server: {
     open: true,
   },
-  base: "/",
 })
