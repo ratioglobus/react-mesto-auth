2b474df0e1537c33159c522cb24285578d066bea
Ivan <racioglobus@yandex.ru>
2024-02-03 20:46:49 +0300

    update repo, p15

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="footer">
-        <p className="link footer__copyright">© 2023 Popov Ivan </p>
-      </footer>
-    )
-}
--- src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children, ...props }) {
-  return props.loggedIn ? children : <Navigate to='/sign-in' replace />;
-};
